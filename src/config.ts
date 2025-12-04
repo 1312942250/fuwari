97f3e4d3641afeb3aa5290f59adf52e19bd577e6
liuyu <1312942250@qq.com>
2025-12-04 09:36:10 +0800

    修改网页标题

--- src/config.ts
+++ src/config.ts
@@ -8,8 +8,8 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "维新派丁真的博客",
-	subtitle: "",
+	title: "淋雨一直走",
+	subtitle: "ly's blog",
 	lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
 		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
